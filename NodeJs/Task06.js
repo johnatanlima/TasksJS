4791e5475b38bfec703f675f4e6863945512035d
userDexter Fedora29 <joh23nathan@gmail.com>
2019-08-30 01:39:29 -0300

    Erro no resultado.. precisa ser corrido

--- NodeJs/Task06.js
+++ NodeJs/Task06.js
@@ -1,11 +1,19 @@
-function obterUsuario(callback){
-    setTimeout(function(){
-        return callback(null, {
-            id:1,
-            nome: 'johw',
-            dataNascimento: new Date()
-        })
-    }, 1000)
+"use strict"
+
+function obterUsuario(){
+    //Se der algo inesperado => reject(ERRO pra ele)
+    //Se passar e for success => resolve()
+    return new Promisse(function resolvePromisse(resolve, reject)
+    {
+        setTimeout(function(){
+            return resolve({
+                id: 1,
+                nome: 'johw',
+                dataNascimento: new Date()
+            })
+        }, 1000)
+    })
+    
 }
 
 function obterTelefone(idUsuario, callback){
@@ -26,8 +34,21 @@ function obterEndereco(idUsuario, callback){
     }, 3000)
 }
 
-function resolverUsuario(erro, usuario){
-    console.log('usuario', usuario)
-}
+//Criando uma variavel obj que vai receber os dados do objeto trazido pela funcao
+const usuarioPromisse = obterUsuario()
+    //obterUser é uma funcao que retorna uma Promisse, e a promisse retorna 2 resultados que ela tratou lá
+    //Para tratar os dados da promisse se for sucesso, usamos o .then
+    //Para tratar os dados da promisse se for um erro, usamos o .catch
+usuarioPromisse().resolvePromisse()
+    .then(function(resultado){ //essa function aqui passa uma callback, pra chamar um resultado que é o log
+        console.log('Resultado... ', resultado) 
+    })
+    .catch(function(error){
+        console.error('Deu ruim, vey... ', error)
+    })
+
+
+
+
 
 
