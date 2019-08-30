dbb9dad0f17779ef016a44952f74b72788ab3203
userDexter Fedora29 <joh23nathan@gmail.com>
2019-08-29 23:56:00 -0300

    Implementando Promisses

--- NodeJs/Task06.js
+++ NodeJs/Task06.js
@@ -8,3 +8,20 @@ function obterUsuario(callback){
     }, 1000)
 }
 
+function obterTelefone(idUsuario, callback){
+    setTimeout(() => {
+        return callback(null, {
+            telefone:'985859164',
+            ddd: '19',
+        })
+    }, 2000)
+}
+
+function obterEndereco(idUsuario, callback){
+    setTimeout(() => {
+        return callback(null, {
+            rua:'dos Santos',
+            numero: 55
+        })
+    }, 3000)
+}
